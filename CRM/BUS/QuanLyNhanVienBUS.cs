7687399f64c0cca58720930c0bca1460e784c288
vnhalowen <45254808+vnhalowen@users.noreply.github.com>
2019-05-24 08:17:52 +0700

    mất tiêu cái Data , buồn :(

---
+++ CRM/BUS/QuanLyNhanVienBUS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BUS
+{
+    public class QuanLyNhanVienBUS
+    {
+        //private QuanLyNhanVienDAO addDAO;
+        //public QuanLyNhanVienBUS()
+        //{
+
+        //}
+    }
+}
---
+++ CRM/DAO/QuanLiNhanVienDAO.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+using System.Data;
+
+namespace DAO
+{
+    public class QuanLiNhanVienDAO : DataProvider
+    {
+        public QuanLiNhanVienDAO():base()
+        { }
+        //public DataTable getNV (QuanLyNhanVienEntities add)
+        //{
+        //    string sql = "insert into QuanLyNhanVien ('ten','gioitinh','thuongtru','tamtru','cmnd','ngaycap','noicap','email','bophan','ngayvao','sdt') values ('ten'='" + add.Ten +'",'" + );
+        //}
+    }
+}
